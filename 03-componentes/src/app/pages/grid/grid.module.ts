a3829c3e99aaf6f93874683e2709df40dd1369f3
Dante Arrighi <dantearrighi@gmail.com>
2023-03-25 23:47:23 -0300

    Grid

---
+++ 03-componentes/src/app/pages/grid/grid.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { ComponentsModule } from '../../components/components.module';
+import { GridPageRoutingModule } from './grid-routing.module';
+import { GridPage } from './grid.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    GridPageRoutingModule,
+    ComponentsModule
+  ],
+  declarations: [GridPage]
+})
+export class GridPageModule { }
