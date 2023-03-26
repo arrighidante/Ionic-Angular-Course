80b7693cd290e8e070d286b77f707d2e2e6e1172
Dante Arrighi <dantearrighi@gmail.com>
2023-03-25 23:37:05 -0300

    Ion-fab

---
+++ 03-componentes/src/app/pages/fab/fab.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { ComponentsModule } from '../../components/components.module';
+import { FabPageRoutingModule } from './fab-routing.module';
+import { FabPage } from './fab.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    FabPageRoutingModule,
+    ComponentsModule
+  ],
+  declarations: [FabPage]
+})
+export class FabPageModule { }
