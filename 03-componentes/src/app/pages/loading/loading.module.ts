f4c0c21abdfe17be4829d7edb83d540739d437c3
Dante Arrighi <dantearrighi@gmail.com>
2023-03-26 01:26:54 -0300

    ion-loading

---
+++ 03-componentes/src/app/pages/loading/loading.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { ComponentsModule } from '../../components/components.module';
+import { LoadingPageRoutingModule } from './loading-routing.module';
+import { LoadingPage } from './loading.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoadingPageRoutingModule,
+    ComponentsModule
+  ],
+  declarations: [LoadingPage]
+})
+export class LoadingPageModule { }
